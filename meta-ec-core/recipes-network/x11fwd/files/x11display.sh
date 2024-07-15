97165cb426b844f0fe6897cde91665f53e627a24
Julien Combattelli <julien.combattelli@gmail.com>
2024-07-15 10:52:04 +0200

    Disable x11 config for WSL2 with WSLg enabled

--- meta-ec-core/recipes-network/x11fwd/files/x11display.sh
+++ meta-ec-core/recipes-network/x11fwd/files/x11display.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+if [ "$(which-platform)" = "wsl2" ] && findmnt /mnt/wslg/ 2>&1 >/dev/null ; then
+    # WSLg enabled on WSL2, X11 forwarding already configured
+    return
+fi
+
 export DISPLAY=@X11_DISPLAY@
 export LIBGL_ALWAYS_INDIRECT=1
