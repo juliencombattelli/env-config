8336316d3557036301b565707edb0c222a3c1230
Julien Combattelli <julien.combattelli@gmail.com>
2024-07-26 10:30:53 +0200

    Reset cursor style in an omz segment due to WT bug

--- meta-ec-core/recipes-shell/zsh/files/p10k.zsh.post.zsh
+++ meta-ec-core/recipes-shell/zsh/files/p10k.zsh.post.zsh
@@ -9,14 +9,14 @@
   emulate -L zsh -o extended_glob
 
   # Add aosp and tmux segment just before context segment (user@hostname)
-  POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS[${POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS[(i)context]}]=(aosp tmux context)
+  POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS[${POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS[(i)context]}]=(aosp tmux set_cursor_style context)
 
   # Override the status Ok symbols from recent p10k versions which is purple
   # with the Hack-NF font for some reasons...
   POWERLEVEL9K_STATUS_OK_VISUAL_IDENTIFIER_EXPANSION='✓'
   POWERLEVEL9K_STATUS_OK_PIPE_VISUAL_IDENTIFIER_EXPANSION='✓'
 
-  function prompt_aosp() {
+  function prompt_aosp {
     local aosp_product
     if [ -z "${ANDROID_BUILD_TOP+x}" ] || [ -z "${TARGET_PRODUCT+x}" ]; then
       return
@@ -24,11 +24,17 @@
     p10k segment -f "darkolivegreen2" -ri "ANDROID_ICON" -t "${TARGET_PRODUCT}"
   }
 
-  function prompt_tmux() {
+  function prompt_tmux {
     if [ -z "$TMUX" ]; then
       return
     fi
     p10k segment -f "green" -t "tmux"
+  }
+
+  function prompt_set_cursor_style {
+    # Workaround for a Windows Terminal bug
+    # https://github.com/microsoft/terminal/issues/10754
+    printf "\e[0 q"
   }
 
   (( ! $+functions[p10k] )) || p10k reload
