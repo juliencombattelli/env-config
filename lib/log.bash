10f242004d84391aa50d646c0385021b8b5b7b9b
Julien Combattelli <julien.combattelli@gmail.com>
2026-02-23 10:31:06 +0100

    Use to a more compact log header

--- lib/log.bash
+++ lib/log.bash
@@ -9,17 +9,17 @@ function ec_log_plain {
 }
 
 function ec_log_debug {
-    printf "[$(_ec_time)] [$(ansi_style fg=cyan)DEBUG$(ansi_style)] %s\n" "$@" >&2
+    printf "$(_ec_time) $(ansi_style fg=cyan)D$(ansi_style) %s\n" "$@" >&2
 }
 
 function ec_log_note {
-    printf "[$(_ec_time)] [$(ansi_style bold fg=white)NOTE $(ansi_style)] %s\n" "$@" >&2
+    printf "$(_ec_time) $(ansi_style bold fg=white)N$(ansi_style) %s\n" "$@" >&2
 }
 
 function ec_log_warn {
-    printf "[$(_ec_time)] [$(ansi_style bold fg=yellow)WARN $(ansi_style)] $(ansi_style bold fg=yellow)%s$(ansi_style)\n" "$@" >&2
+    printf "$(_ec_time) $(ansi_style bold fg=yellow)W$(ansi_style) $(ansi_style bold fg=yellow)%s$(ansi_style)\n" "$@" >&2
 }
 
 function ec_log_error {
-    printf "[$(_ec_time)] [$(ansi_style bold fg=red)ERROR$(ansi_style)] $(ansi_style bold fg=red)%s$(ansi_style)\n" "$@" >&2
+    printf "$(_ec_time) $(ansi_style bold fg=red)E$(ansi_style) $(ansi_style bold fg=red)%s$(ansi_style)\n" "$@" >&2
 }
