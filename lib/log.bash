7586067b1dcf3c590b237cd05307aa015a2ec464
Julien Combattelli <julien.combattelli@gmail.com>
2026-02-23 19:21:14 +0100

    Factorize logger functions

--- lib/log.bash
+++ lib/log.bash
@@ -1,6 +1,7 @@
 source ansi.bash
 
 EC_LOG_COLOR=true
+EC_LOG_DIR=
 
 function _ec_log_ansi_style {
     if $EC_LOG_COLOR; then
@@ -12,22 +13,51 @@ function _ec_time {
     printf "%(%Y-%m-%d %H:%M:%S)T" -1
 }
 
+function _ec_log_level_P {
+    :
+}
+
+function _ec_log_level_D {
+    printf " %sD%s" "$(_ec_log_ansi_style fg=cyan)" "$(_ec_log_ansi_style)"
+}
+
+function _ec_log_level_N {
+    printf " %sN%s" "$(_ec_log_ansi_style bold fg=white)" "$(_ec_log_ansi_style)"
+}
+
+function _ec_log_level_W {
+    printf " %sW%s" "$(_ec_log_ansi_style bold fg=yellow)" "$(_ec_log_ansi_style)"
+}
+
+function _ec_log_level_E {
+    printf " %sE%s" "$(_ec_log_ansi_style bold fg=red)" "$(_ec_log_ansi_style)"
+}
+
+function _ec_log {
+    local level="$1"
+    shift
+    printf "$(_ec_time)$(_ec_log_level_"$level") %s\n" "$@" >&2
+    if [[ -d "$EC_LOG_DIR" ]]; then
+        printf "$(_ec_time) $level %s\n" "$@" >> "$EC_LOG_DIR/env-config.log"
+    fi
+}
+
 function ec_log_plain {
-    printf "%s\n" "$@" >&2
+    _ec_log P "$@"
 }
 
 function ec_log_debug {
-    printf "$(_ec_time) $(_ec_log_ansi_style fg=cyan)D$(_ec_log_ansi_style) %s\n" "$@" >&2
+    _ec_log D "$@"
 }
 
 function ec_log_note {
-    printf "$(_ec_time) $(_ec_log_ansi_style bold fg=white)N$(_ec_log_ansi_style) %s\n" "$@" >&2
+    _ec_log N "$@"
 }
 
 function ec_log_warn {
-    printf "$(_ec_time) $(_ec_log_ansi_style bold fg=yellow)W$(_ec_log_ansi_style) $(_ec_log_ansi_style bold fg=yellow)%s$(_ec_log_ansi_style)\n" "$@" >&2
+    _ec_log W "$@"
 }
 
 function ec_log_error {
-    printf "$(_ec_time) $(_ec_log_ansi_style bold fg=red)E$(_ec_log_ansi_style) $(_ec_log_ansi_style bold fg=red)%s$(_ec_log_ansi_style)\n" "$@" >&2
+    _ec_log E "$@"
 }
