c73d45277e1b08f80c3f026ceb9c6aa5a19ea2c6
Julien Combattelli <julien.combattelli@gmail.com>
2026-07-02 15:27:23 +0200

    Fix many coding style deviations

--- lib/utils.bash
+++ lib/utils.bash
@@ -10,3 +10,19 @@ function ec_contains {
     done
     return 1
 }
+
+function ec_join {
+    local delim=${1-} f=${2-}
+    if shift 2; then
+        printf %s "$f" "${@/#/$delim}"
+    fi
+}
+
+function ec_join_affix {
+    local delim=${1-} pre=${2-} post=${3-}
+    if shift 3; then
+        set -- "${@/#/$pre}"
+        set -- "${@/%/$post}"
+        ec_join "$delim" "$@"
+    fi
+}
