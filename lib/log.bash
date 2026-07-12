36e687d9bf6388336af7c778be215d69295209ac
Julien Combattelli <julien.combattelli@gmail.com>
2026-07-12 16:01:41 +0200

    Rework log module to support logging to file and stdout simultaneously

--- lib/log.bash
+++ lib/log.bash
@@ -2,7 +2,7 @@ source ansi.bash
 
 EC_LOG_COLOR=true
 EC_LOG_FILE=env-config.log
-EC_LOG_TARGETS=($EC_LOG_FILE)
+EC_LOG_TARGETS=("$EC_LOG_FILE")
 
 function _ec_log_ansi_style {
     if $ec_log_color; then
@@ -34,43 +34,32 @@ function _ec_log_level_E {
     printf " %sE%s" "$(_ec_log_ansi_style bold fg=red)" "$(_ec_log_ansi_style)"
 }
 
-function _ec_log {
-    function _ec_log_set_output {
-        ec_log_color=$EC_LOG_COLOR
-        case "$1" in
-            stdout) exec {LOG_FD}>&1;;
-            stderr) exec {LOG_FD}>&2;;
-            *)      exec {LOG_FD}>>"$1"; ec_log_color=false;;
-        esac
-    }
-    function _ec_log_close_output {
-        exec {LOG_FD}>&-
-    }
-    local level="$1"
+function _ec_log_set_output {
+    ec_log_color=$EC_LOG_COLOR
+    case "$1" in
+        stdout) exec {LOG_FD}>&1;;
+        stderr) exec {LOG_FD}>&2;;
+        file)   exec {LOG_FD}>>"$EC_LOG_FILE"; ec_log_color=false;;
+        *)      exec {LOG_FD}>>"$1"; ec_log_color=false;;
+    esac
+}
+
+function _ec_log_close_output {
+    exec {LOG_FD}>&-
+}
+
+function ec_log {
+    local spec="$1"
     shift
-    for target in "${EC_LOG_TARGETS[@]}"; do
+
+    local level targets_spec targets
+    IFS='@' read -r level targets_spec <<< "$spec"
+    IFS=',' read -ra targets <<< "$targets_spec"
+
+    local default_targets=(file stdout)
+    for target in "${targets[@]:-${default_targets[@]}}"; do
         _ec_log_set_output "$target"
         printf "$(_ec_time)$(_ec_log_level_"$level") %s\n" "$@" >&"$LOG_FD"
         _ec_log_close_output
     done
-}
-
-function ec_log_plain {
-    _ec_log P "$@"
-}
-
-function ec_log_debug {
-    _ec_log D "$@"
-}
-
-function ec_log_note {
-    _ec_log N "$@"
-}
-
-function ec_log_warn {
-    _ec_log W "$@"
-}
-
-function ec_log_error {
-    _ec_log E "$@"
 }
