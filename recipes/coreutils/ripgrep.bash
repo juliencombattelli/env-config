f3cd117b4bd0210d29c25a3e5d64ab7c67d6312e
Julien Combattelli <julien.combattelli@gmail.com>
2026-07-29 16:07:51 +0200

    Finish ripgrep installation

--- recipes/coreutils/ripgrep.bash
+++ recipes/coreutils/ripgrep.bash
@@ -1,3 +1,9 @@
 function ec_do_install {
-    echo "Installing ripgrep!"
+    ec_download_latest_github_release "BurntSushi/ripgrep" "${EC_CPU_ARCH}.*linux-musl.*\.tar\.gz$"
+
+    tar -xf "$D/latest"
+
+    cd "$(dirname "$(find . -name rg)")" || return 1
+
+    cp rg ~/.local/bin/
 }
