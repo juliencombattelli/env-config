b37df5eea158edd9ea147ffeb1052bf9f0dd45ed
Julien Combattelli <julien.combattelli@gmail.com>
2026-08-08 16:02:53 +0200

    Implement homebrew pkgprov interface

--- pkg_providers/homebrew.bash
+++ pkg_providers/homebrew.bash
@@ -4,15 +4,19 @@ function ec_homebrew_post_prereq_setup {
 
 function ec_homebrew_pkg_search {
     local -r pattern="$1"
-    false
+    brew formulae \
+        | grep -E "$pattern" \
+        | sort --reverse --human-numeric-sort \
+        | head --lines=1
 }
 
 function ec_homebrew_pkg_installed {
     local -r pattern="$1"
-    false
+    brew list --formulae -1 \
+        | grep -E "$pattern"
 }
 
 function ec_homebrew_pkg_install {
     local -r pkg="$1"
-    false
+    brew install --yes "$pkg"
 }
