eba46e2c6aafc2a7a40e1af8dc20729954e569c4
Julien Combattelli <julien.combattelli@gmail.com>
2026-08-08 15:30:16 +0200

    Remove fedora atomic conditionals from dnf pkgprov

--- pkg_providers/dnf.bash
+++ pkg_providers/dnf.bash
@@ -1,38 +1,22 @@
 function ec_dnf_setup {
-    if $EC_FEDORA_ATOMIC; then
-        true
-    else
-        sudo -E dnf update --assumeyes
-    fi
+    : #sudo -E dnf update --assumeyes
 }
 
 function ec_dnf_pkg_search {
     local -r pattern="$1"
-    if $EC_FEDORA_ATOMIC; then
-        false
-    else
-        dnf --quiet repoquery --queryformat '%{name}\n' '*' \
-            | grep -E "$pattern" \
-            | sort --reverse --human-numeric-sort \
-            | head --lines=1
-    fi
+    dnf --quiet repoquery --queryformat '%{name}\n' '*' \
+        | grep -E "$pattern" \
+        | sort --reverse --human-numeric-sort \
+        | head --lines=1
 }
 
 function ec_dnf_pkg_installed {
     local -r pattern="$1"
-    if $EC_FEDORA_ATOMIC; then
-        false
-    else
-        rpm --query --all --queryformat='%{NAME}\n' \
-            | grep -E "$pattern"
-    fi
+    rpm --query --all --queryformat='%{NAME}\n' \
+        | grep -E "$pattern"
 }
 
 function ec_dnf_pkg_install {
     local -r pkg="$1"
-    if $EC_FEDORA_ATOMIC; then
-        false
-    else
-        sudo -E dnf install --assumeyes "$pkg"
-    fi
+    sudo -E dnf install --assumeyes "$pkg"
 }
