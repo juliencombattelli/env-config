7f4a1a7a0d78f3f389581106118882b1e8aa709b
Julien Combattelli <julien.combattelli@gmail.com>
2026-08-09 09:41:22 +0200

    Add neovim recipe

--- recipes/editor/neovim.bash
+++ recipes/editor/neovim.bash
@@ -1,5 +1,15 @@
 EC_DEPENDS+=(libfuse2)
 
 function ec_do_install {
-    echo "Installing neovim (stubbed)"
+    ec_download_latest_github_release "neovim/neovim" "linux.*${EC_CPU_ARCH}.*\.appimage$"
+
+    cp "$D/latest" ~/.local/bin/nvim
+    chmod +x ~/.local/bin/nvim
+
+    if [ ! -d ~/.config/nvim ]; then
+        ec_log N "Downloading and installing Neovim configuration."
+        git clone https://github.com/juliencombattelli/neovim-config ~/.config/nvim -b v2
+    else
+        ec_log N "Neovim configuration already installed, nothing to do."
+    fi
 }
