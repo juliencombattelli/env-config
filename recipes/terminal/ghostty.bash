1a27f336725580f3b865cd77d897daabd8c754f8
Julien Combattelli <julien.combattelli@gmail.com>
2026-08-12 15:52:53 +0200

    Fix Ghostty installation

--- recipes/terminal/ghostty.bash
+++ recipes/terminal/ghostty.bash
@@ -2,9 +2,9 @@ function ec_do_install {
     ec_download_latest_github_release "pkgforge-dev/ghostty-appimage" ".*${EC_CPU_ARCH}.*\.AppImage$"
 
     mkdir -p ~/.local/share/applications
-    ec_relink ~/.local/share/applications/Ghostty.desktop ~/.config/ghostty/Ghostty.desktop
+    ec_relink ~/.local/share/applications/com.mitchellh.ghostty.desktop ~/.config/env-config/files/ghostty/com.mitchellh.ghostty.desktop
 
     cp "$D/latest" ~/.local/bin/ghostty.appimage
     chmod +x ~/.local/bin/ghostty.appimage
-    ec_relink ~/.local/bin/ ~/.config/ghostty/ghostty
+    ec_relink ~/.local/bin/ghostty ~/.local/bin/ghostty.appimage
 }
